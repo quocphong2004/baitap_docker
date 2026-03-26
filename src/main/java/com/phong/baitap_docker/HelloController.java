864312f765b0f8bb12629ed09b24106f88aafdab
Phong2004-beep99 <buifhong@gmail.com>
2026-03-26 14:38:36 +0700

    sync

--- src/main/java/com/phong/baitap_docker/HelloController.java
+++ src/main/java/com/phong/baitap_docker/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello docker : 2280602340_Bùi Quốc Phong";
+        return "Bài tập CI  Hello docker : 2280602340_Bùi Quốc Phong ";
     }
 
 }
